3c62775bc7325d2025298ea6b14279ce687f297a
ION <43673238+yasukiion@users.noreply.github.com>
2023-02-18 23:20:46 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 import streamlit as st
 # 別のファイルで定義された関数をインポート
-from app import process_data
+from app import model
 
 def process_data(selected_files):
-    print(f"Received data: name={selected_files}")
+    print(f"Received data: name={model.selected_files}")
 
 process_data()
 
