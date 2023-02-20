253d013b2d17e3fd4538c0d941835ea06a1e8e9a
ION <43673238+yasukiion@users.noreply.github.com>
2023-02-20 23:17:11 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
 import streamlit as st
 # 別のファイルで定義された関数をインポート
-from app import model
 from PIL import Image
 import numpy as np
 
 def process_data(selected_files):
-    print(f"Received data: name={model.selected_files}")
+    print(f"Received data: name={selected_files}")
     for selected_file in selected_files:
         image = Image.open(selected_file).convert('L')  # convert to grayscale
         data = np.array(image)  # convert to numpy array
