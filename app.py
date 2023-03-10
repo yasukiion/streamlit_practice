29925ca07100b92d46e2661b64ea0352a46f92b1
ION <43673238+yasukiion@users.noreply.github.com>
2023-03-10 16:42:05 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,86 +1,116 @@
-import streamlit as st
-import os
-from PIL import Image
-import shutil
-import image_explore
-import model
-from model import cnn
-from image2dataset import image_2_dataset
-from zipfile import ZipFile
-import tensorflow as tf
+from flask import Flask
 
-# フォルダのパスを指定
-#予測用のフォルダ
-UPLOAD_FOLDER = "/Users/nobutair/ceremony/uploads"
-#学習用
-DATA_FOLDER = "./ceremony/data"
-#モデルの場所
-MODEL_FOLDER = "./ceremony/my_model"
+def create_app():
+    app = Flask(__name__)
+    # add routes, templates, and other configuration here
+    return app
 
-def save_uploaded_file(uploaded_file):
-    # フォルダが存在しない場合は作成する
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
-    # ファイルを保存する
-    file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return file_path
-
-def main():
-    #cnnのモデルに学習させる
-    st.title("モデルに学習させますか？")
-    if st.button("Click me"):
-      cnn(DATA_FOLDER,MODEL_FOLDER)
-      st.write("学習完了しました。cnnモデルは最新です")
-    st.title("結婚式場の画像をアップロードしてください")
-    # フォルダをアップロードする
-    folder = st.file_uploader("Upload a folder", type="zip")
-    if folder:
-        # zipファイルを展開する
-        path = os.path.join(os.getcwd(), folder.name)
-        with open(path, "wb") as f:
-            f.write(folder.getbuffer())
-        os.makedirs(os.path.join(os.getcwd(), UPLOAD_FOLDER), exist_ok=True)
-        shutil.unpack_archive(path, os.path.join(os.getcwd(),UPLOAD_FOLDER))
-        st.write(path)
-
-        #予測用のデータが入っているフォルダ
-        PREDICTION_FOLDER = path
-
-        # フォルダ内の画像を表示する
-        image_extensions = ["jpg", "jpeg", "png"]
-        image_folder = st.sidebar.selectbox("Select a folder", os.listdir(os.path.join(os.getcwd(),UPLOAD_FOLDER)))
-        for file_name in os.listdir(os.path.join(os.path.join(os.getcwd(),UPLOAD_FOLDER, image_folder))):
-            if file_name.split(".")[-1] in image_extensions:
-                image_path = os.path.join(os.path.join(os.path.join(os.getcwd(),UPLOAD_FOLDER, image_folder)), file_name)
-                image = Image.open(image_path)
-                st.image(image, caption=file_name, use_column_width=True)
-                file_path = save_uploaded_file(folder)
-                st.write("Saved file:", file_path)
-
-
-        # Zipファイルを展開する
-        with ZipFile(folder, "r") as zip:
-          zip.extractall(UPLOAD_FOLDER)
-          os.remove("./ceremony/uploads/名称未設定フォルダ.zip")
-        shutil.rmtree("./ceremony/uploads/__MACOSX")
-
-        # サブミットボタンでフォームをサブミットする
-        form = st.form(key='my-form')
-        submit_button = form.form_submit_button('Submit')
-        if submit_button:
-            #入力した画像をモデルが読み込める形に変える
-            data = image_explore.get_image_files(UPLOAD_FOLDER)
-            pre_dataset = image_2_dataset(UPLOAD_FOLDER)
-            # モデルを読み込む
-            loaded_model = tf.keras.models.load_model(MODEL_FOLDER)
-            # モデルを使用して予測を行う
-            #エポック数いらないよね？
-            predictions = loaded_model.predict(pre_dataset)
-            #csvに変換
-            predictions.to_csv("predictions.csv",index=False)
-
-if __name__ == "__main__":
-    main()
-
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "eb64501e-24be-44f7-b9f5-cd6fa510c840",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " * Serving Flask app \"__main__\" (lazy loading)\n",
+      " * Environment: production\n",
+      "\u001b[31m   WARNING: This is a development server. Do not use it in a production deployment.\u001b[0m\n",
+      "\u001b[2m   Use a production WSGI server instead.\u001b[0m\n",
+      " * Debug mode: on\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n",
+      " * Restarting with watchdog (fsevents)\n",
+      "Traceback (most recent call last):\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/ipykernel_launcher.py\", line 16, in <module>\n",
+      "    app.launch_new_instance()\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/traitlets/config/application.py\", line 845, in launch_instance\n",
+      "    app.initialize(argv)\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/traitlets/config/application.py\", line 88, in inner\n",
+      "    return method(app, *args, **kwargs)\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/ipykernel/kernelapp.py\", line 632, in initialize\n",
+      "    self.init_sockets()\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/ipykernel/kernelapp.py\", line 282, in init_sockets\n",
+      "    self.shell_port = self._bind_socket(self.shell_socket, self.shell_port)\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/ipykernel/kernelapp.py\", line 229, in _bind_socket\n",
+      "    return self._try_bind_socket(s, port)\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/ipykernel/kernelapp.py\", line 205, in _try_bind_socket\n",
+      "    s.bind(\"tcp://%s:%i\" % (self.ip, port))\n",
+      "  File \"/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/zmq/sugar/socket.py\", line 214, in bind\n",
+      "    super().bind(addr)\n",
+      "  File \"zmq/backend/cython/socket.pyx\", line 540, in zmq.backend.cython.socket.Socket.bind\n",
+      "  File \"zmq/backend/cython/checkrc.pxd\", line 28, in zmq.backend.cython.checkrc._check_rc\n",
+      "zmq.error.ZMQError: Address already in use\n"
+     ]
+    },
+    {
+     "ename": "SystemExit",
+     "evalue": "1",
+     "output_type": "error",
+     "traceback": [
+      "An exception has occurred, use %tb to see the full traceback.\n",
+      "\u001b[0;31mSystemExit\u001b[0m\u001b[0;31m:\u001b[0m 1\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "/Users/nobutair/opt/anaconda3/lib/python3.9/site-packages/IPython/core/interactiveshell.py:3377: UserWarning: To exit: use 'exit', 'quit', or Ctrl-D.\n",
+      "  warn(\"To exit: use 'exit', 'quit', or Ctrl-D.\", stacklevel=1)\n"
+     ]
+    }
+   ],
+   "source": [
+    "from flask import Flask\n",
+    "\n",
+    "app = Flask(__name__)\n",
+    "\n",
+    "@app.route(\"/\")\n",
+    "def home():\n",
+    "    return \"Hello, World!\"\n",
+    "\n",
+    "if __name__ == '__main__':\n",
+    "    app.run(debug=True)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": None,
+   "id": "a71e5236-f3fc-4a1e-8419-868613bde1c2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
