6353418f64d8bf61dba91eed526299c6b58746ea
ION <43673238+yasukiion@users.noreply.github.com>
2023-03-11 11:00:15 +0900

    Add files via upload

--- image_check.py
+++ image_check.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import os
+from PIL import Image
 
 #フォルダ内の画像を表示する
-def image_check(UPLOAD_FOLDER,PREDICTION_ZIP):
+def image_check(UPLOAD_FOLDER,PREDICTION_ZIP,PREDICTION_FOLDER):
         image_extensions = ["jpg", "jpeg", "png"]
-        image_folder = st.sidebar.selectbox("Select a folder", os.listdir(os.path.join(os.getcwd(),UPLOAD_FOLDER)))
-        for file_name in os.listdir(os.path.join(os.path.join(os.getcwd(),UPLOAD_FOLDER, image_folder))):
+        #image_folder = st.sidebar.selectbox("Select a folder", os.listdir(os.path.join(os.getcwd(),UPLOAD_FOLDER)))
+        for file_name in os.listdir(os.path.join(os.path.join(os.getcwd(),UPLOAD_FOLDER, PREDICTION_FOLDER))):
             if file_name.split(".")[-1] in image_extensions:
-                image_path = os.path.join(os.path.join(os.path.join(os.getcwd(),UPLOAD_FOLDER, image_folder)), file_name)
+                image_path = os.path.join(os.path.join(os.path.join(os.getcwd(),UPLOAD_FOLDER, PREDICTION_FOLDER)), file_name)
                 image = Image.open(image_path)
                 st.image(image, caption=file_name, use_column_width=True)
                 st.write("Saved file:", PREDICTION_ZIP)
