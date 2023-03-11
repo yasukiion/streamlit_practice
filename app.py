928edf80c02f0b1d021f633f7774fe272be6987d
ION <43673238+yasukiion@users.noreply.github.com>
2023-03-11 10:54:04 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ import datetime
 
 # フォルダのパスを指定
 #予測用のフォルダ
-UPLOAD_FOLDER = "/ceremony/uploads"
+UPLOAD_FOLDER = "nobutair/ceremony/uploads"
 #学習用
-DATA_FOLDER = "/ceremony/data"
+DATA_FOLDER = "nobutair/ceremony/data"
 #モデルの場所
-MODEL_FOLDER = "/ceremony/my_model"
+MODEL_FOLDER = "nobutair/ceremony/my_model"
 #なぜか作られてしまうゴミフォルダ
-MACOSX = "/ceremony/uploads/__MACOSX"
+MACOSX = "nobutair/ceremony/uploads/__MACOSX"
 #csvファイルを保存するフォルダ
 # 現在の日付を取得する
 today = datetime.date.today()
